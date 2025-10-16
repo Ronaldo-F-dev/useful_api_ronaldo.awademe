f2735de06436d4d9f9f7d121448256f83251008a
Ronaldo <awademeronaldoo@gmail.com>
2025-10-16 10:54:40 +0100

    [CREATE] Module model with her factory && seeder

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,5 @@ Route::get("/",function(){
 
 Route::post("/register",[AuthController::class,"register"]);
 Route::post("/login",[AuthController::class, "login"]);
+
+Route::get("/modules",[ModuleController::class,"index"]);
