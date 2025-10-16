43875cc7152671b80e8c21428daf84b816b3ccb9
Ronaldo <awademeronaldoo@gmail.com>
2025-10-16 10:06:15 +0100

    [UPDATE] bootstrap/app.php file to enable using api routes.

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,12 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get("/",function(){
+    return [
+        "success" => true,
+        "version" => "1.0.0",
+        "description" => "Useful API"
+    ];
+});
